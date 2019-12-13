297396f27c2efaeea7ab4f664d0a24e926b837e1
Ahmed Khalaf <ahmedkhalaf.92@gmail.com>
2019-12-13 21:42:30 +0200

    handle non-square imageViews

--- MaskedImageView/MaskedImageView.swift
+++ MaskedImageView/MaskedImageView.swift
@@ -11,14 +11,16 @@ import UIKit
 class MaskedImageView: UIImageView {
     private lazy var maskLayer: CALayer = {
         let l = CAShapeLayer()
+        l.frame = self.layer.bounds
         l.backgroundColor = UIColor.clear.cgColor
+        
         let sideLength = min(self.bounds.size.width, self.bounds.size.height) / 2
-        let rect = CGRect(origin: CGPoint(x: sideLength / 2, y: sideLength / 2), size: CGSize(width: sideLength, height: sideLength))
+        let center = convert(self.center, from: self.superview!)
+        let rect = CGRect(origin: center.applying(CGAffineTransform(translationX: -sideLength / 2, y: -sideLength / 2)), size: CGSize(width: sideLength, height: sideLength))
         l.path = UIBezierPath(roundedRect: rect, cornerRadius: sideLength / 4).cgPath
-        var transform = CATransform3DMakeTranslation(-sideLength, -sideLength, 0)
-        transform = CATransform3DConcat(transform, CATransform3DMakeRotation(.pi / 4, 0, 0, 1))
-        transform = CATransform3DConcat(transform, CATransform3DMakeTranslation(sideLength, sideLength, 0))
-        l.transform = transform
+        
+        l.transform = CATransform3DMakeRotation(.pi / 4, 0, 0, 1)
+        
         return l
     }()
     
