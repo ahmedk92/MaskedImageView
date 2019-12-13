74f90ee494f815ba7f1b11861c9127fd84016241
Ahmed Khalaf <ahmedkhalaf.92@gmail.com>
2019-12-13 20:10:08 +0200

    Initial Commit

---
+++ MaskedImageView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MaskedImageView
+//
+//  Created by Ahmed Khalaf on 12/13/19.
+//  Copyright © 2019 Ahmed Khalaf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
